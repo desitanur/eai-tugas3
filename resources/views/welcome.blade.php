b47c6b0586378632d63b6664f006970c62f3eda4
Desita Nur R <desitarosyidiana@gmail.com>
2020-09-28 00:04:08 +0700

    test praktikum

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Ayo, semangat DESITA :)
+                    DATA BUKU
                 </div>
 
                 <div class="links">
